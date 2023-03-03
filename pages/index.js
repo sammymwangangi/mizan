1408731067e6b8cfcbff59601320b14343fc7a60
Sammy <sammymwinzi2@gmail.com>
2023-03-03 17:52:43 +0300

    restore modals

--- pages/index.js
+++ pages/index.js
@@ -781,6 +781,62 @@ export default function Home() {
         {/* End */}
       </main>
 
+      {/* models */}
+
+      <div
+        className="modal mobile-menu"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-fullscreen-sm-down">
+          <div className="modal-content">
+            <button
+              type="button"
+              className="close-btn"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            >
+              <Image src={Images.closeBtn} width="20px" alt="" />
+            </button>
+            <div className="modal-body">
+              <div className="logo">
+                <Image src={Images.mobileLogo} alt="" />
+              </div>
+              <div className="mobile-menu-list mt-5">
+                <ul>
+                  <li>
+                    <Link className="active" href="/">
+                      Personal
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/brand-story">Brand Story</Link>
+                  </li>
+                </ul>
+              </div>
+              <button className="join-btn my-2">JOIN WAITLIST</button>
+              <div className="mobile-list mt-5">
+                <ul>
+                  <li>
+                    <a className="active" href="#">
+                      Terms and Conditions
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy Policy</a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy Notice</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <Script src="/vendor/mySwiper.js" />
       <Script src="/vendor/featureSwiper.js" />
 
